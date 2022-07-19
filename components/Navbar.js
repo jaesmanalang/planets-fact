7180eff4db427a62bad4b3bd8c683f075eaa56ce
James Manalang <james.e.manalang@gmail.com>
2022-07-19 23:06:59 +0800

    Setup tailwindcss, assets, and google fonts

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="flex">
+      <div className="h-[85px] bg-transparent font-heading font-medium text-[28px] uppercase">
+        The Planets
+      </div>
+    </nav>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: {
+    fontFamily: {
+      heading: ['Antonio'],
+      body: ["'League Spartan'"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
